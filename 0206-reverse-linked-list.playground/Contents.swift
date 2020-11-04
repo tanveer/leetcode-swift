ce3b00ba58ec8387ceff4a5eba427fd3230b3867
Tanveer Bashir <tan.m.bashir@gmail.com>
2020-11-04 15:26:00 -0500

    Solution #206 Reverse Linked List

---
+++ 0206-reverse-linked-list.playground/Contents.swift
@@ -0,0 +1,134 @@
+import Foundation
+
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list
+
+// MARK: - Solution
+// ================
+// Definition for singly-linked list.
+public class ListNode: ExpressibleByArrayLiteral, ExpressibleByStringLiteral {
+    public var val: Int
+    public var next: ListNode?
+
+    public convenience init() { self.init(0) }
+    public convenience init(_ val: Int) { self.init(val, nil) }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next }
+
+    init?(_ values: [Int]) {
+        guard let firstVal = values.first else { return nil }
+        self.val = firstVal
+        self.append(contentsOf: values.dropFirst())
+    }
+
+    // Allow initialization as: let listNode: ListNode = "1->2->3"
+    public required convenience init(stringLiteral: String) {
+        let values = stringLiteral.replacingOccurrences(of: " ", with: "").components(separatedBy: "->").map { Int($0)! }
+        guard let firstVal = values.first else { fatalError("Invalid Input String. Expecting Format: 1->2->3") }
+        self.init(firstVal)
+        self.append(contentsOf: values.dropFirst())
+    }
+
+    // Allow initialization as: let listNode: ListNode = [1, 2, 3]
+    public required convenience init(arrayLiteral: Int...) {
+        guard let firstVal = arrayLiteral.first else { fatalError("Cannot be used on empty arrays since they should represent nil") }
+        self.init(firstVal)
+        self.append(contentsOf: arrayLiteral.dropFirst())
+    }
+
+    private func append<S: Sequence>(contentsOf values: S) where S.Iterator.Element == Int {
+        var current = self
+        while let next = current.next {
+            current = next
+        }
+
+        for num in values {
+            let newNode = ListNode(num)
+            current.next = newNode
+            current = newNode
+        }
+    }
+}
+
+// This is to allow XCTAssertEqual to compare two LinkedLists
+extension ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        var nullableLhs: ListNode? = lhs
+        var nullableRhs: ListNode? = rhs
+
+        while let l = nullableLhs, let r = nullableRhs {
+            guard l.val == r.val else { return false }
+            nullableLhs = l.next
+            nullableRhs = r.next
+        }
+
+        return nullableLhs == nil && nullableRhs == nil
+    }
+}
+
+// To allow displaying the LinkedList in format: 1 -> 2 -> 3
+extension ListNode: CustomStringConvertible {
+    public var description: String {
+        return asString()
+    }
+
+    func asString() -> String {
+        return asArray().map { String($0) }.joined(separator: " -> ")
+    }
+
+    func asArray() -> [Int] {
+        var values = [Int]()
+        var node: ListNode? = self
+        while let current = node {
+            values.append(current.val)
+            node = current.next
+        }
+        return values
+    }
+}
+
+// MARK: - Solution
+// ================
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+
+        guard head != nil || head?.next != nil else { return head }
+
+        var curr = head
+        var prev: ListNode? = nil
+
+        while curr != nil {
+            let next = curr?.next
+            curr?.next = prev
+            prev = curr
+            curr = next
+        }
+        curr = prev
+        return curr
+    }
+}
+
+// MARK: - Time and Space Complexity
+// =================================
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+// MARK: - Tests
+// =============
+import XCTest
+class Tests: XCTestCase {
+    let s = Solution()
+
+    // LeetCode Examples
+    func testLeetCodeExample() {
+        XCTAssertEqual(s.reverseList("1->2->3->4->5"), "5->4->3->2->1")
+    }
+
+    // Additional Examples
+    func testAdditionalExamples() {
+        XCTAssertEqual(s.reverseList(nil), nil)
+    }
+}
+
+Tests.defaultTestSuite.run()
+
