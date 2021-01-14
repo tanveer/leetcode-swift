aaf3375b953a29a9cbd1e0da7b861a98067c4cc3
Tanveer Bashir <tan.m.bashir@gmail.com>
2021-01-14 16:56:08 -0500

    Solution #328 Odd Even Linked List - https://leetcode.com/problems/odd-even-linked-list

---
+++ 0328-odd-even-linked-list.playground/Contents.swift
@@ -0,0 +1,136 @@
+import Foundation
+
+// 328. Odd Even Linked List
+// https://leetcode.com/problems/odd-even-linked-list
+
+// MARK: - Solution
+// ================
+class Solution {
+    func oddEvenList(_ head: ListNode?) -> ListNode? {
+        guard let head = head else { return nil }
+        var odd: ListNode? = head
+        var even: ListNode? = head.next
+        let evenHead: ListNode? = even
+
+        while let node = even, let next = even?.next {
+            odd?.next = node.next
+            odd = next
+            even?.next = odd?.next
+            even = odd?.next
+        }
+
+        odd?.next = evenHead
+
+        return head
+    }
+}
+
+// Definition for singly-linked list.
+public class ListNode: ExpressibleByArrayLiteral, ExpressibleByStringLiteral {
+    public var val: Int
+    public var next: ListNode?
+
+    public convenience init() { self.init(0) }
+    public convenience init(_ val: Int) { self.init(val, nil) }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next }
+
+    init?(_ values: [Int]) {
+        guard let firstVal = values.first else { return nil }
+        self.val = firstVal
+        self.append(contentsOf: values.dropFirst())
+    }
+
+    // Allow initialization as: let listNode: ListNode = "1->2->3"
+    public required convenience init(stringLiteral: String) {
+        let values = stringLiteral.replacingOccurrences(of: " ", with: "").components(separatedBy: "->").map { Int($0)! }
+        guard let firstVal = values.first else { fatalError("Invalid Input String. Expecting Format: 1->2->3") }
+        self.init(firstVal)
+        self.append(contentsOf: values.dropFirst())
+    }
+
+    // Allow initialization as: let listNode: ListNode = [1, 2, 3]
+    public required convenience init(arrayLiteral: Int...) {
+        guard let firstVal = arrayLiteral.first else { fatalError("Cannot be used on empty arrays since they should represent nil") }
+        self.init(firstVal)
+        self.append(contentsOf: arrayLiteral.dropFirst())
+    }
+
+    private func append<S: Sequence>(contentsOf values: S) where S.Iterator.Element == Int {
+        var current = self
+        while let next = current.next {
+            current = next
+        }
+
+        for num in values {
+            let newNode = ListNode(num)
+            current.next = newNode
+            current = newNode
+        }
+    }
+}
+
+// This is to allow XCTAssertEqual to compare two LinkedLists
+extension ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        var nullableLhs: ListNode? = lhs
+        var nullableRhs: ListNode? = rhs
+
+        while let l = nullableLhs, let r = nullableRhs {
+            guard l.val == r.val else { return false }
+            nullableLhs = l.next
+            nullableRhs = r.next
+        }
+
+        return nullableLhs == nil && nullableRhs == nil
+    }
+}
+
+// To allow displaying the LinkedList in format: 1 -> 2 -> 3
+extension ListNode: CustomStringConvertible {
+    public var description: String {
+        return asString()
+    }
+
+    func asString() -> String {
+        return asArray().map { String($0) }.joined(separator: " -> ")
+    }
+
+    func asArray() -> [Int] {
+        var values = [Int]()
+        var node: ListNode? = self
+        while let current = node {
+            values.append(current.val)
+            node = current.next
+        }
+        return values
+    }
+}
+
+// MARK: - Time and Space Complexity
+// =================================
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+// MARK: - Tests
+// =============
+import XCTest
+class Tests: XCTestCase {
+    let s = Solution()
+
+    // LeetCode Examples
+    func testLeetCodeExample1() {
+        XCTAssertEqual(s.oddEvenList("1->2->3->4->5"), "1->3->5->2->4")
+    }
+
+    func testLeetCodeExample2() {
+        XCTAssertEqual(s.oddEvenList("2->1->3->5->6->4->7"), "2->3->6->7->1->5->4")
+    }
+
+    // Additional Examples
+    func testAdditionalExamples() {
+        XCTAssertNil(s.oddEvenList(nil))
+        XCTAssertEqual(s.oddEvenList("1->2->3->4->5->6"), "1->3->5->2->4->6")
+    }
+}
+
+Tests.defaultTestSuite.run()
