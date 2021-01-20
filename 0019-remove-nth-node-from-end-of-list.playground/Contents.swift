a8460e286f940ef4496c42fb7b195bdfcf5407e1
Tanveer Bashir <tan.m.bashir@gmail.com>
2021-01-20 18:52:18 -0500

    Solution #19 Remove Nth Node From End of List - https://leetcode.com/problems/remove-nth-node-from-end-of-list

---
+++ 0019-remove-nth-node-from-end-of-list.playground/Contents.swift
@@ -0,0 +1,140 @@
+import Foundation
+
+// 19. Remove Nth Node From End of List
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list
+
+class Solution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        guard head != nil else { return nil }
+        var fast = head
+        var slow = head
+
+        for _ in 1...n {
+            fast = fast?.next
+        }
+
+        if fast == nil {
+            return head?.next
+        }
+
+        while let _ = fast?.next {
+            fast = fast?.next
+            slow = slow?.next
+        }
+        slow?.next = slow?.next?.next
+
+        return head
+    }
+}
+
+// Definition for singly-linked list.
+public class ListNode: ExpressibleByArrayLiteral, ExpressibleByStringLiteral {
+    public var val: Int
+    public var next: ListNode?
+
+    public convenience init() { self.init(0) }
+    public convenience init(_ val: Int) { self.init(val, nil) }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next }
+
+    init?(_ values: [Int]) {
+        guard let firstVal = values.first else { return nil }
+        self.val = firstVal
+        self.append(contentsOf: values.dropFirst())
+    }
+
+    // Allow initialization as: let listNode: ListNode = "1->2->3"
+    public required convenience init(stringLiteral: String) {
+        let values = stringLiteral.replacingOccurrences(of: " ", with: "").components(separatedBy: "->").map { Int($0)! }
+        guard let firstVal = values.first else { fatalError("Invalid Input String. Expecting Format: 1->2->3") }
+        self.init(firstVal)
+        self.append(contentsOf: values.dropFirst())
+    }
+
+    // Allow initialization as: let listNode: ListNode = [1, 2, 3]
+    public required convenience init(arrayLiteral: Int...) {
+        guard let firstVal = arrayLiteral.first else { fatalError("Cannot be used on empty arrays since they should represent nil") }
+        self.init(firstVal)
+        self.append(contentsOf: arrayLiteral.dropFirst())
+    }
+
+    private func append<S: Sequence>(contentsOf values: S) where S.Iterator.Element == Int {
+        var current = self
+        while let next = current.next {
+            current = next
+        }
+
+        for num in values {
+            let newNode = ListNode(num)
+            current.next = newNode
+            current = newNode
+        }
+    }
+}
+
+// This is to allow XCTAssertEqual to compare two LinkedLists
+extension ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        var nullableLhs: ListNode? = lhs
+        var nullableRhs: ListNode? = rhs
+        while let l = nullableLhs, let r = nullableRhs {
+            guard l.val == r.val else { return false }
+            nullableLhs = l.next
+            nullableRhs = r.next
+        }
+        return nullableLhs == nil && nullableRhs == nil
+    }
+}
+
+// To allow displaying the LinkedList in format: 1 -> 2 -> 3
+extension ListNode: CustomStringConvertible {
+    public var description: String {
+        return asString()
+    }
+
+    func asString() -> String {
+        return asArray().map { String($0) }.joined(separator: " -> ")
+    }
+
+    func asArray() -> [Int] {
+        var values = [Int]()
+        var node: ListNode? = self
+        while let current = node {
+            values.append(current.val)
+            node = current.next
+        }
+        return values
+    }
+}
+
+// MARK: - Time and Space Complexity
+// =================================
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+// MARK: - Tests
+// =============
+import XCTest
+class Tests: XCTestCase {
+    let s = Solution()
+
+    // LeetCode Examples
+    func testLeetCodeExample1() {
+        XCTAssertEqual(s.removeNthFromEnd([1, 2, 3, 4, 5], 2), [1, 2, 3, 5])
+    }
+
+    func testLeetCodeExample2() {
+        XCTAssertNil(s.removeNthFromEnd([1], 1))
+    }
+
+    func testLeetCodeExample3() {
+        XCTAssertEqual(s.removeNthFromEnd([1, 2], 1), [1])
+    }
+
+    // Additional Examples
+    func testAdditionalExamples() {
+        XCTAssertNil(s.removeNthFromEnd(nil, 1))
+        XCTAssertEqual(s.removeNthFromEnd([1, 2, 3], 3), [2, 3]) // Remove Head
+    }
+}
+
+Tests.defaultTestSuite.run()
