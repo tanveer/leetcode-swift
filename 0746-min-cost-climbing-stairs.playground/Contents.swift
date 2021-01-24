0716c435da7b24b84e266525c56d1bd9f4e2e51d
Tanveer Bashir <tan.m.bashir@gmail.com>
2021-01-24 16:41:43 -0500

    Solution #746 Min Cost Climbing Stairs - https://leetcode.com/problems/min-cost-climbing-stairs

--- 0746-min-cost-climbing-stairs.playground/Contents.swift
+++ 0746-min-cost-climbing-stairs.playground/Contents.swift
@@ -9,39 +9,41 @@ class Solution {
     func minCostClimbingStairs(_ cost: [Int]) -> Int {
         var a = 0
         var b = 0
-        
+
         for n in cost {
             let c = n + min(a, b)
             b = a
             a = c
         }
-        
+
         return min(a, b)
     }
 }
-
 
 // MARK: - Time and Space Complexity
 // =================================
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 
-
 // MARK: - Tests
 // =============
 import XCTest
 class Tests: XCTestCase {
     let s = Solution()
-    
+
     // LeetCode Examples
     func testLeetCodeExample1() {
         XCTAssertEqual(s.minCostClimbingStairs([10, 15, 20]), 15)
     }
-    
+
     func testLeetCodeExample2() {
         XCTAssertEqual(s.minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]), 6)
+    }
+
+    // Additional Examples
+    func testAdditionalExamples() {
+        XCTAssertEqual(s.minCostClimbingStairs([]), 0)
     }
 }
 
 Tests.defaultTestSuite.run()
-
