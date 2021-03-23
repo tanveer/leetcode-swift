65f9b7da72b928a62be8aca6a3d60f5647d9bf4d
Tanveer Bashir <tan.m.bashir@gmail.com>
2021-03-23 16:27:34 -0400

    Solution #965 Univalued Binary Tree - https://leetcode.com/problems/univalued-binary-tree

---
+++ 0965-univalued-binary-tree.playground/Contents.swift
@@ -0,0 +1,111 @@
+import Foundation
+
+// 965. Univalued Binary Tree
+// https://leetcode.com/problems/univalued-binary-tree
+
+// MARK: - Solution
+// ================
+class Solution {
+    var values = [Int]()
+
+    func isUnivalTree(_ root: TreeNode?) -> Bool {
+        dfs(root)
+        return Set<Int>(values).count == 1
+    }
+
+    func dfs(_ node: TreeNode?) {
+        guard let node = node else { return }
+        self.values.append(node.val)
+        dfs(node.left)
+        dfs(node.right)
+    }
+}
+
+// MARK: - Supporting Code: Definition for a binary tree node
+// ==========================================================
+public class TreeNode: ExpressibleByArrayLiteral, ExpressibleByIntegerLiteral, Equatable {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+
+    public init() { self.val = 0; self.left = nil; self.right = nil }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+
+    // ExpressibleByIntegerLiteral
+    // Allow left and right leaves to be specified as integers, i.e. TreeNode(1, 2, 3) vs TreeNode(1, TreeNode(2), TreeNode(3))
+    public required convenience init(integerLiteral value: Int) {
+        self.init(value)
+    }
+
+    // ExpressibleByArrayLiteral
+    // Allow initialization as: let treeNode: TreeNode = [1, 2, 3]
+    public required convenience init(arrayLiteral: Int?...) {
+        guard let firstVal = arrayLiteral.first else { fatalError("Cannot be used on empty arrays since they should represent nil") }
+        guard let rootNodeVal = firstVal else { fatalError("Root node's value cannot be nil") }
+        self.init(rootNodeVal)
+
+        var queueArray: [TreeNode] = [self]
+        var valuesToAdd = Array(arrayLiteral.dropFirst())
+
+        while !queueArray.isEmpty, !valuesToAdd.isEmpty {
+            let node = queueArray.removeFirst()
+
+            if let leftVal = valuesToAdd.removeFirst() {
+                node.left = TreeNode(leftVal)
+                queueArray.append(node.left!)
+            }
+
+            guard !valuesToAdd.isEmpty else { return }
+            if let rightVal = valuesToAdd.removeFirst() {
+                node.right = TreeNode(rightVal)
+                queueArray.append(node.right!)
+            }
+        }
+    }
+
+    // Equatable
+    // This is to allow XCTAssertEqual to compare two TreeNodes
+    public static func == (lhs: TreeNode, rhs: TreeNode) -> Bool {
+        return isSameTree(lhs, rhs)
+    }
+
+    static func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil, q == nil {
+            return true
+        } else if p?.val == q?.val {
+            return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+        } else {
+            return false
+        }
+    }
+}
+
+
+// MARK: - Time and Space Complexity
+// =================================
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
+// MARK: - Tests
+// =============
+import XCTest
+class Tests: XCTestCase {
+    let s = Solution()
+
+    // LeetCode Examples
+    func testLeetCodeExample1() {
+        XCTAssertTrue(s.isUnivalTree([1, 1, 1, 1, 1, nil, 1]))
+    }
+
+    func testLeetCodeExample2() {
+        XCTAssertFalse(s.isUnivalTree([2, 2, 2, 5, 2]))
+    }
+}
+
+Tests.defaultTestSuite.run()
