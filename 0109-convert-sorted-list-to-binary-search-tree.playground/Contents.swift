9ee4328a632673f36bc9140b48d26697002b4c4b
Tanveer Bashir <tan.m.bashir@gmail.com>
2021-04-11 17:57:00 -0400

    Solution #109 Convert Sorted List to Binary Search Tree - https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree

---
+++ 0109-convert-sorted-list-to-binary-search-tree.playground/Contents.swift
@@ -0,0 +1,219 @@
+import Foundation
+
+// 109. Convert Sorted List to Binary Search Tree
+// https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree
+
+// MARK: - Solution
+// ================
+class Solution {
+    func sortedListToBST(_ head: ListNode?) -> TreeNode? {
+        guard head != nil else { return nil }
+
+        let mid = findMid(head)
+        let root = TreeNode(mid!.val)
+        guard head !== mid else { return root }
+
+        root.left = sortedListToBST(head)
+        root.right = sortedListToBST(mid?.next)
+        return root
+    }
+
+    func findMid(_ node: ListNode?) -> ListNode? {
+        var fast = node
+        var slow = node
+        var prev = node
+        while fast != nil, fast?.next != nil {
+            prev = slow
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+
+        if prev != nil {
+            prev?.next = nil
+        }
+        return slow
+    }
+}
+
+// Definition for singly-linked list.
+public class ListNode: ExpressibleByArrayLiteral, ExpressibleByStringLiteral {
+    public var val: Int
+    public var next: ListNode?
+
+    public convenience init() { self.init(0) }
+    public convenience init(_ val: Int) { self.init(val, nil) }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next }
+
+    init?(_ values: [Int]) {
+        guard let firstVal = values.first else { return nil }
+        self.val = firstVal
+        self.append(contentsOf: values.dropFirst())
+    }
+
+    // Allow initialization as: let listNode: ListNode = "1->2->3"
+    public required convenience init(stringLiteral: String) {
+        let values = stringLiteral.replacingOccurrences(of: " ", with: "").components(separatedBy: "->").map { Int($0)! }
+        guard let firstVal = values.first else { fatalError("Invalid Input String. Expecting Format: 1->2->3") }
+        self.init(firstVal)
+        self.append(contentsOf: values.dropFirst())
+    }
+
+    // Allow initialization as: let listNode: ListNode = [1, 2, 3]
+    public required convenience init(arrayLiteral: Int...) {
+        guard let firstVal = arrayLiteral.first else { fatalError("Cannot be used on empty arrays since they should represent nil") }
+        self.init(firstVal)
+        self.append(contentsOf: arrayLiteral.dropFirst())
+    }
+
+    private func append<S: Sequence>(contentsOf values: S) where S.Iterator.Element == Int {
+        var current = self
+        while let next = current.next {
+            current = next
+        }
+
+        for num in values {
+            let newNode = ListNode(num)
+            current.next = newNode
+            current = newNode
+        }
+    }
+}
+
+// This is to allow XCTAssertEqual to compare two LinkedLists
+extension ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        var nullableLhs: ListNode? = lhs
+        var nullableRhs: ListNode? = rhs
+
+        while let l = nullableLhs, let r = nullableRhs {
+            guard l.val == r.val else { return false }
+            nullableLhs = l.next
+            nullableRhs = r.next
+        }
+
+        return nullableLhs == nil && nullableRhs == nil
+    }
+}
+
+// To allow displaying the LinkedList in format: 1 -> 2 -> 3
+extension ListNode: CustomStringConvertible {
+    public var description: String {
+        return asString()
+    }
+
+    func asString() -> String {
+        return asArray().map { String($0) }.joined(separator: " -> ")
+    }
+
+    func asArray() -> [Int] {
+        var values = [Int]()
+        var node: ListNode? = self
+        while let current = node {
+            values.append(current.val)
+            node = current.next
+        }
+        return values
+    }
+}
+
+// MARK: - Supporting Code: Definition for a binary tree node
+// ==========================================================
+public class TreeNode: ExpressibleByArrayLiteral, ExpressibleByIntegerLiteral, Equatable {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+
+    public init() { self.val = 0; self.left = nil; self.right = nil }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+
+    // ExpressibleByIntegerLiteral
+    // Allow left and right leaves to be specified as integers, i.e. TreeNode(1, 2, 3) vs TreeNode(1, TreeNode(2), TreeNode(3))
+    public required convenience init(integerLiteral value: Int) {
+        self.init(value)
+    }
+
+    // ExpressibleByArrayLiteral
+    // Allow initialization as: let treeNode: TreeNode = [1, 2, 3]
+    public required convenience init(arrayLiteral: Int?...) {
+        guard let firstVal = arrayLiteral.first else { fatalError("Cannot be used on empty arrays since they should represent nil") }
+        guard let rootNodeVal = firstVal else { fatalError("Root node's value cannot be nil") }
+        self.init(rootNodeVal)
+
+        var queueArray: [TreeNode] = [self]
+        var valuesToAdd = Array(arrayLiteral.dropFirst())
+
+        while !queueArray.isEmpty, !valuesToAdd.isEmpty {
+            let node = queueArray.removeFirst()
+
+            if let leftVal = valuesToAdd.removeFirst() {
+                node.left = TreeNode(leftVal)
+                queueArray.append(node.left!)
+            }
+
+            guard !valuesToAdd.isEmpty else { return }
+            if let rightVal = valuesToAdd.removeFirst() {
+                node.right = TreeNode(rightVal)
+                queueArray.append(node.right!)
+            }
+        }
+    }
+
+    // Equatable
+    // This is to allow XCTAssertEqual to compare two TreeNodes
+    public static func == (lhs: TreeNode, rhs: TreeNode) -> Bool {
+        return isSameTree(lhs, rhs)
+    }
+
+    static func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil, q == nil {
+            return true
+        } else if p?.val == q?.val {
+            return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+        } else {
+            return false
+        }
+    }
+}
+
+
+// MARK: - Time and Space Complexity
+// =================================
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+// MARK: - Tests
+// =============
+import XCTest
+class Tests: XCTestCase {
+    let s = Solution()
+
+    // LeetCode Examples
+    func testLeetCodeExample1() {
+        XCTAssertEqual(s.sortedListToBST([-10, -3, 0, 5, 9]), [0, -3, 9, -10, nil, 5])
+    }
+
+    func testLeetCodeExample2() {
+        XCTAssertNil(s.sortedListToBST(nil))
+    }
+
+    func testLeetCodeExample3() {
+        XCTAssertEqual(s.sortedListToBST([0]), [0])
+    }
+
+    func testLeetCodeExample4() {
+        XCTAssertEqual(s.sortedListToBST([1, 3]), [3, 1])
+    }
+
+    // Additional Examples
+    func testAdditionalExamples() {
+        XCTAssertNil(s.sortedListToBST(nil))
+    }
+}
+
+Tests.defaultTestSuite.run()
