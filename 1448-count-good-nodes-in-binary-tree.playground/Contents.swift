806ae508069b405f2d87987355f965d417170f47
Tanveer Bashir <tan.m.bashir@gmail.com>
2021-07-08 14:44:34 -0400

    Solution #1448 Count Good Nodes in Binary Tree - https://leetcode.com/problems/count-good-nodes-in-binary-tree

---
+++ 1448-count-good-nodes-in-binary-tree.playground/Contents.swift
@@ -0,0 +1,124 @@
+import Foundation
+
+// 1448. Count Good Nodes in Binary Tree
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree
+
+// MARK: - Solution
+// ================
+class Solution {
+    func goodNodes(_ root: TreeNode?) -> Int {
+        return dfs(root, Int.min)
+    }
+
+    func dfs(_ root: TreeNode?, _ value: Int) -> Int {
+        guard let root = root else { return 0 }
+        var count = 0
+
+        if root.val >= value {
+            count += 1
+        }
+        let maxValue = max(value, root.val)
+
+        count += dfs(root.left, maxValue)
+        count += dfs(root.right, maxValue)
+        return count
+    }
+}
+
+// MARK: - Supporting Code: Definition for a binary tree node
+// ==========================================================
+public class TreeNode: ExpressibleByArrayLiteral, ExpressibleByIntegerLiteral, Equatable {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+
+    public init() { self.val = 0; self.left = nil; self.right = nil }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+
+    // ExpressibleByIntegerLiteral
+    // Allow left and right leaves to be specified as integers, i.e. TreeNode(1, 2, 3) vs TreeNode(1, TreeNode(2), TreeNode(3))
+    public required convenience init(integerLiteral value: Int) {
+        self.init(value)
+    }
+
+    // ExpressibleByArrayLiteral
+    // Allow initialization as: let treeNode: TreeNode = [1, 2, 3]
+    public required convenience init(arrayLiteral: Int?...) {
+        guard let firstVal = arrayLiteral.first else { fatalError("Cannot be used on empty arrays since they should represent nil") }
+        guard let rootNodeVal = firstVal else { fatalError("Root node's value cannot be nil") }
+        self.init(rootNodeVal)
+
+        var queueArray: [TreeNode] = [self]
+        var valuesToAdd = Array(arrayLiteral.dropFirst())
+
+        while !queueArray.isEmpty, !valuesToAdd.isEmpty {
+            let node = queueArray.removeFirst()
+
+            if let leftVal = valuesToAdd.removeFirst() {
+                node.left = TreeNode(leftVal)
+                queueArray.append(node.left!)
+            }
+
+            guard !valuesToAdd.isEmpty else { return }
+            if let rightVal = valuesToAdd.removeFirst() {
+                node.right = TreeNode(rightVal)
+                queueArray.append(node.right!)
+            }
+        }
+    }
+
+    // Equatable
+    // This is to allow XCTAssertEqual to compare two TreeNodes
+    public static func == (lhs: TreeNode, rhs: TreeNode) -> Bool {
+        return isSameTree(lhs, rhs)
+    }
+
+    static func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil, q == nil {
+            return true
+        } else if p?.val == q?.val {
+            return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+        } else {
+            return false
+        }
+    }
+}
+
+
+// MARK: - Time and Space Complexity
+// =================================
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+// MARK: - Tests
+// =============
+import XCTest
+class Tests: XCTestCase {
+    let s = Solution()
+
+    // LeetCode Examples
+    func testLeetCodeExample1() {
+        XCTAssertEqual(s.goodNodes([3, 1, 4, 3, nil, 1, 5]), 4)
+    }
+
+    func testLeetCodeExample2() {
+        XCTAssertEqual(s.goodNodes([3, 3, nil, 4, 2]), 3)
+    }
+
+    func testLeetCodeExample3() {
+        XCTAssertEqual(s.goodNodes([1]), 1)
+    }
+
+    // Additional Examples
+    func testAdditionalExamples() {
+        XCTAssertEqual(s.goodNodes(nil), 0)
+    }
+}
+
+Tests.defaultTestSuite.run()
