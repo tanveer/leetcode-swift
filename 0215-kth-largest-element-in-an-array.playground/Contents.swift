91315416e30bbea939a72a3359d575ed523e6174
Tanveer Bashir <tan.m.bashir@gmail.com>
2021-07-10 17:42:51 -0400

    Solution #215 Kth Largest Element in an Array - https://leetcode.com/problems/kth-largest-element-in-an-array

--- 0215-kth-largest-element-in-an-array.playground/Contents.swift
+++ 0215-kth-largest-element-in-an-array.playground/Contents.swift
@@ -7,16 +7,120 @@ import Foundation
 // ================
 class Solution {
     func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
-        guard !nums.isEmpty else { return 0 }
-        return nums.sorted { $0 > $1 }[k - 1] // sort in reverse order and return k - 1
+        var heap = Heap()
+
+        for n in nums {
+            heap.add(n)
+        }
+
+        var count = heap.count - k
+        while count != 0 {
+            heap.poll()
+            count -= 1
+        }
+
+        return heap.top ?? -1
+    }
+}
+
+struct Heap {
+    private var items = [Int]()
+
+    var count: Int {
+        return items.count
+    }
+
+    var top: Int? {
+        return items.first
+    }
+
+    private func getLeftChildIndex(_ index: Int) -> Int {
+        return 2 * index + 1
+    }
+
+    private func getRightChildIndex(_ index: Int) -> Int {
+        return 2 * index + 2
+    }
+
+    private func getParentIndex(_ childIndex: Int) -> Int {
+        return (childIndex - 1) / 2
+    }
+
+    private func hasLeftChild(_ index: Int) -> Bool {
+        return getLeftChildIndex(index) < items.count
+    }
+
+    private func hasRightChild(_ index: Int) -> Bool {
+        return getRightChildIndex(index) < items.count
+    }
+
+    private func hasParent(_ index: Int) -> Bool {
+        return getParentIndex(index) >= 0
+    }
+
+
+    // access nodes
+    private func leftChild(_ index: Int) -> Int {
+        return items[getLeftChildIndex(index)]
+    }
+
+    private func rightChild(_ index: Int) -> Int {
+        return items[getRightChildIndex(index)]
+    }
+
+    private func parent(_ index: Int) -> Int {
+        return items[getParentIndex(index)]
+    }
+
+    private mutating func bubbleUp() {
+        var index = count - 1
+        while hasParent(index), parent(index) > items[index] {
+            let parentIndex = getParentIndex(index)
+            items.swapAt(parentIndex, index)
+            index = parentIndex
+        }
+    }
+
+    private mutating func bubbleDown() {
+        var index = 0
+        while hasLeftChild(index) {
+            var leftChildIndex = getLeftChildIndex(index)
+
+            if hasRightChild(index), rightChild(index) < leftChild(index) {
+                leftChildIndex = getRightChildIndex(index)
+            }
+
+            if items[index] > items[leftChildIndex] {
+                items.swapAt(index, leftChildIndex)
+            }
+            index = leftChildIndex
+        }
+    }
+
+    public mutating func add(_ item: Int) {
+        items.append(item)
+        bubbleUp()
+    }
+
+    public func peek() -> Int? {
+        return top
+    }
+
+    public mutating func poll() -> Int? {
+        guard top != nil else { return nil }
+        let item = top
+        items[0] = items[count - 1]
+        bubbleDown()
+        items.removeLast()
+        return item
     }
 }
 
 
 // MARK: - Time and Space Complexity
 // =================================
-// Time Complexity: O(n log(n))
-// Space Complexity: O(1)
+// Time Complexity: O(nlogk)
+// Space Complexity: O(k)
 
 
 // MARK: - Tests
